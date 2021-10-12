8a34608b4d52cfead9d62386c502ef12077a2ff2
asb1512 <andrew@olympicmedia.com>
2021-10-11 20:37:29 -0400

    added setup for Auth0 and React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
+import { Route, Switch} from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+
+import NavBar from './components/NavBar';
+import Loading from './components/Loading';
+
 import './App.css';
 
 function App() {
+
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <div className="App">
       
+
+      <Switch>
+        <Route path="/" component={NavBar} />
+      </Switch>
     </div>
   );
 }
