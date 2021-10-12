e2cad70393394532edbca43a58694a2cd29a9629
asb1512 <andrew@olympicmedia.com>
2021-10-11 23:30:26 -0400

    created Dashboard

---
+++ src/views/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function Dashboard() {
+  const { user } = useAuth0();
+  const { email } = user;
+
+  return (
+    <div>
+      Welcome {email}!
+    </div>
+  )
+}
