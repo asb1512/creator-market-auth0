558404e6d7774b526cc63841fd17172ad60bd00f
asb1512 <andrew@olympicmedia.com>
2021-10-21 23:20:39 -0400

    added two cases to reducer to handle calls to Twitch API*

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -9,6 +9,18 @@ function reducer(state = defaultState, action) {
         ...state,
         currentUser: action.payload
       }
+
+    case "OAUTH_TWITCH_LOADING":
+      return {
+        ...state,
+        currentUser: {
+          twitch: { loading: true }
+        }
+      }
+
+    case "OAUTH_TWITCH":
+      return
+
     default: return state
   }
 }
