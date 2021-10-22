79f126976c3a772f8fb47ac270664838a9de7e95
asb1512 <andrew@olympicmedia.com>
2021-10-21 23:18:36 -0400

    created action to validate all future requests to Twitch API

---
+++ src/actions/twitchValidateOauth.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export function twitchValidateOauth() {
+  return function (dispatch) {
+    dispatch({type: "OAUTH_TWITCH_LOADING"})
+    return axios({
+      method: 'get',
+      url: 'https://id.twitch.tv/oauth2/validate',
+      headers: {'Authorization': `${process.env.REACT_APP_TWITCH_ACCESS_TOKEN}`},
+    })
+    .then(function (response) {
+      console.log("response:", response)
+    })
+    .catch(function (error) {
+      console.log("error:", error)
+    })
+  }
+}
