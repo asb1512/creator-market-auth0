4c701ce63760e3045e2096af53bb2679295bf63f
asb1512 <andrew@olympicmedia.com>
2021-10-21 23:19:47 -0400

    created Dashboard to be redirected to after success user authentication, displaying all platforms

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
+import NavBar from '../components/NavBar';
+import Platforms from '../components/Platforms';
+
 export default function Dashboard() {
   const { user } = useAuth0();
   const { email } = user;
 
   return (
     <div>
+      <NavBar />
       Welcome {email}!
+      <Platforms />
     </div>
   )
 }
