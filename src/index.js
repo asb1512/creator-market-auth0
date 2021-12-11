1a87380596fbbc7eb115fa7e4f09f59d8855b1ee
asb1512 <andrew@olympicmedia.com>
2021-12-11 18:08:33 -0500

    commented out function for Redux Chrome extension

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
 
-const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composedEnhancer = compose(applyMiddleware(thunk))
 const store = createStore(reducer, undefined, composedEnhancer)
 
 ReactDOM.render(
